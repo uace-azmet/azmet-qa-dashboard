cad53e4775e0d153db67c3a29241f308352d01f4
Eric R. Scott <scottericr@gmail.com>
2023-07-17 10:53:33 -0400

    data validator version with custom gt table

--- app-data_validator/R/check_daily.R
+++ app-data_validator/R/check_daily.R
@@ -1,41 +1,66 @@
-library(azmetr)
-# library(data.validator)
-# daily <- az_daily(start = "2023-07-01", end = "2023-07-12")
-
-check_daily2 <- function(daily) {
-report <- data_validation_report()
-  
+#' Create validation report for daily data
+#'
+#' @param daily data frame from azmetr::az_daily()
+#'
+#' @return a data.validator report
+#' @examples
+#' library(azmetr)
+#' library(data.validator)
+#' daily <- az_daily(start = "2023-07-01", end = "2023-07-12")
+#' check_daily(daily)
+#' 
+check_daily <- function(daily) {
+  report <- data_validation_report()
   validate(daily, name = "Daily Data") |>
     # Internal consistency checks from 'NWS (1994) TSP 88-21-R2':
-    validate_if(temp_air_meanC >= dwpt_mean, "Mean air temp ≥ dewpoint") |> 
-    validate_if(temp_air_minC <= temp_air_meanC, "min air temp ≤ mean air temp") |> 
-    validate_if(temp_air_meanC <= temp_air_maxC, "mean air temp ≤ max air temp") |> 
-    validate_if(wind_spd_mean_mps <= wind_spd_max_mps, "mean wind speed ≤ max wind speed") |> 
-    validate_if(temp_soil_10cm_meanC <= temp_soil_10cm_maxC, "mean soil temp at 10cm ≤ max soil temp at 10cm") |> 
-    validate_if(temp_soil_10cm_minC <=temp_soil_10cm_meanC, "min soil temp at 10cm ≤ mean soil temp at 10cm") |>
-    # validate_if(temp_soil_50cm_meanC<=temp_soil_50cm_maxC , "mean soil temp at 50cm ≤ max soil temp at 50cm") |>
-    # validate_if(temp_soil_50cm_minC <=temp_soil_50cm_meanC, "min soil temp at 50cm ≤ mean soil temp at 50cm") |>
-    # validate_if(relative_humidity_mean <= relative_humidity_max, "mean RH ≤ max RH") |> 
-    validate_rows(\(x) between(x$relative_humidity_mean, x$relative_humidity_min, x$relative_humidity_max), \(x) isTRUE(ifelse(is.na(x), TRUE, x)), everything(), description = "min ≤ mean ≤ max for RH") |> 
-    validate_rows(\(x) between(x$temp_soil_50cm_meanC, x$temp_soil_50cm_minC, x$temp_soil_50cm_maxC), \(x) isTRUE(ifelse(is.na(x), TRUE, x)), everything(), description = "min ≤ mean ≤ max for soil temp at 50cm") |> 
+    validate_if(gte(temp_air_meanC, dwpt_mean, na_pass = TRUE),
+                "`temp_air_meanC` ≥ `dwpt_mean`") |> 
+    validate_if(
+      btwn(
+        temp_air_meanC,
+        temp_air_minC,
+        temp_air_maxC,
+        na_pass = TRUE
+      ),
+      description = "`temp_air_*` (min ≤ mean ≤ max)"
+    ) |> 
+    validate_if(
+      lte(
+        wind_spd_mean_mps,
+        wind_spd_max_mps,
+        na_pass = TRUE
+      ),
+      description = "`wind_spd_mean_mps` ≤ `wind_spd_max_mps`"
+    ) |> 
+    validate_if(
+      btwn(
+        relative_humidity_mean,
+        relative_humidity_min,
+        relative_humidity_max,
+        na_pass = TRUE
+      ),
+      description = "`relative_humidity_*` (min ≤ mean ≤ max)"
+    ) |> 
+    validate_if(
+      btwn(
+        temp_soil_50cm_meanC,
+        temp_soil_50cm_minC,
+        temp_soil_50cm_maxC,
+        na_pass = TRUE
+      ),
+      description = "`temp_soil_50cm_*` (min ≤ mean ≤ max)"
+    ) |> 
+    validate_if(
+      btwn(
+        temp_soil_10cm_meanC,
+        temp_soil_10cm_minC,
+        temp_soil_10cm_maxC,
+        na_pass = TRUE
+      ),
+      description = "`temp_soil_10cm_*` (min ≤ mean ≤ max)"
+    ) |> 
     add_results(report)
-  
-  
-
-  #   col_vals_gte(temp_air_meanC, vars(dwpt_mean), na_pass = TRUE) |>
-  #   col_vals_lte(temp_air_minC, vars(temp_air_meanC), na_pass = TRUE) |>
-  #   col_vals_lte(temp_air_meanC, vars(temp_air_maxC), na_pass = TRUE) |>
-  #   col_vals_lte(wind_spd_mean_mps, vars(wind_spd_max_mps), na_pass = TRUE) |>
-  #   col_vals_lte(temp_soil_10cm_meanC, vars(temp_soil_10cm_maxC), na_pass = TRUE) |>
-  #   col_vals_lte(temp_soil_10cm_minC, vars(temp_soil_10cm_meanC), na_pass = TRUE) |>
-  #   col_vals_lte(temp_soil_50cm_meanC, vars(temp_soil_50cm_maxC), na_pass = TRUE) |>
-  #   col_vals_lte(temp_soil_50cm_minC, vars(temp_soil_50cm_meanC), na_pass = TRUE) |>
-  #   col_vals_lte(relative_humidity_mean,
-  #                vars(relative_humidity_max),
-  #                na_pass = TRUE) |>
-  #   col_vals_lte(relative_humidity_min,
-  #                vars(relative_humidity_mean),
-  #                na_pass = TRUE)
+  report
   
 }
 
--- app-data_validator/app.R
+++ app-data_validator/app.R
@@ -1,92 +1,39 @@
 library(shiny)
 library(data.validator)
-library(magrittr)
-library(assertr)
-daily <- daily |> 
-  mutate(wind_spd_mean_mps = if_else(meta_station_name == "Tucson", wind_spd_mean_mps + 2000, wind_spd_mean_mps))
-
-# gte(x,y, na_pass = TRUE)
+library(tidyverse)
+library(azmetr)
+library(gt)
 
 ui <- fluidPage(
-  uiOutput("validation")
+  uiOutput("daily_range"),
+  gt::gt_output("validation")
 )
 
 server <- function(input, output, session) {
-  gte <- function(x,y, na_pass = FALSE) {
-    res <- x >= y
-    if(isTRUE(na_pass)) {
-      res <- if_else(is.na(res), TRUE, res)
-    }
-    res
-  }
+  output$daily_range <- renderUI({
+    dateRangeInput(
+      "dailyrange",
+      "Date Range",
+      min = ymd("2020-12-30"),
+      max = Sys.Date(),
+      start = Sys.Date() - 14,
+      end = Sys.Date()
+    )
+  })
   
-  lte <- function(x,y, na_pass = FALSE) {
-    res <- x <= y
-    if(isTRUE(na_pass)) {
-      res <- if_else(is.na(res), TRUE, res)
-    }
-    res
-  }
-  btwn <- function(x, low, high, na_pass = FALSE) {
-    res <- between(x, low, high)
-    if(isTRUE(na_pass)) {
-      res <- if_else(is.na(res), TRUE, res)
-    }
-    res
-  }
-  report <- data_validation_report()
-  
-  validate(daily, name = "Daily Data") |>
-    # Internal consistency checks from 'NWS (1994) TSP 88-21-R2':
-    validate_if(gte(temp_air_meanC, dwpt_mean, na_pass = TRUE), "Mean air temp ≥ dewpoint") |> 
-    validate_if(
-      btwn(
-        temp_air_meanC,
-        temp_air_minC,
-        temp_air_maxC,
-        na_pass = TRUE
-      ),
-      description = "air temp min ≤ mean ≤ max"
-    ) |> 
-    validate_if(
-      lte(
-        wind_spd_mean_mps,
-        wind_spd_max_mps,
-        na_pass = TRUE
-      ),
-      description = "mean wind speed ≤ max wind speed"
-    ) |> 
-    validate_if(
-      btwn(
-        relative_humidity_mean,
-        relative_humidity_min,
-        relative_humidity_max,
-        na_pass = TRUE
-      ),
-      description = "RH min ≤ mean ≤ max"
-    ) |> 
-    validate_if(
-      btwn(
-        temp_soil_50cm_meanC,
-        temp_soil_50cm_minC,
-        temp_soil_50cm_maxC,
-        na_pass = TRUE
-      ),
-      description = "soil temp at 50cm min ≤ mean ≤ max"
-    ) |> 
-    validate_if(
-      btwn(
-        temp_soil_10cm_meanC,
-        temp_soil_10cm_minC,
-        temp_soil_10cm_maxC,
-        na_pass = TRUE
-      ),
-      description = "soil temp at 10cm min ≤ mean ≤ max"
-    ) |> 
-    add_results(report)
-  
-  output$validation <- renderUI({
-    render_semantic_report_ui(get_results(report))
+  output$validation <- gt::render_gt({
+    req(input$dailyrange) #wait until input exists
+    
+    start <- input$dailyrange[1]
+    end <- input$dailyrange[2]
+    #query API
+    daily <- az_daily(start_date = start, end_date = end)
+    
+    #do validation report
+    report <- check_daily(daily)
+    
+    #convert to gt table
+    format_report_gt(report, daily)
   })
 }
 
