6f356806fe65cfdf965eaa4288f0eb38ef60f5c0
Eric R. Scott <scottericr@gmail.com>
2023-07-18 10:58:50 -0400

    helper for checking temporal consistency

--- app-data_validator/R/check_daily.R
+++ app-data_validator/R/check_daily.R
@@ -88,3 +88,24 @@ btwn <- function(x, low, high, na_pass = FALSE) {
   res
 }
 
+
+#' Validate changes
+#' 
+# 'Checks for changes in an numeric vector `x` that are greater than some
+# `delta`.  If |x[i] - x[i-1]|  <= `delta` it returns `FALSE` for *both*
+# indexes.  The point of this is to get both rows to get extracted in the report
+# so you can evaluate the change.
+#' @param x a numeric vector
+#' @param delta maximum allowed change in x from one step to the next
+#'
+#' @return logical vector
+#'
+#' @examples
+#' x <- c(1,2,3,4,10,11,12,15)
+#' check_delta(x, 1)
+check_delta <- function(x, delta) {
+  out <- abs(x - lag(x)) <= delta & abs(x - lead(x)) <= delta
+  #NAs pass
+  out[is.na(out)] <- TRUE
+  out
+}
