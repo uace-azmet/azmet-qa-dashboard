fef3e0a348b574d9c074f6322ce08348206a0d9b
Eric R. Scott <scottericr@gmail.com>
2023-07-19 10:39:39 -0400

    use bslib layout

---
+++ app-bslib/R/check_daily.R
@@ -0,0 +1,64 @@
+#' Create validation report for daily data
+#'
+#' @param daily data frame from azmetr::az_daily()
+#'
+#' @return a data.validator report
+#' @examples
+#' library(azmetr)
+#' library(data.validator)
+#' daily <- az_daily(start = "2023-07-01", end = "2023-07-12")
+#' check_daily(daily)
+check_daily <- function(daily) {
+  report <- data.validator::data_validation_report()
+  data.validator::validate(daily, name = "Daily Data") |>
+    # Internal consistency checks from 'NWS (1994) TSP 88-21-R2':
+    data.validator::validate_if(gte(temp_air_meanC, dwpt_mean, na_pass = TRUE),
+                "`temp_air_meanC` ≥ `dwpt_mean`") |>
+    data.validator::validate_if(
+      btwn(
+        temp_air_meanC,
+        temp_air_minC,
+        temp_air_maxC,
+        na_pass = TRUE
+      ),
+      description = "`temp_air_*` (min ≤ mean ≤ max)"
+    ) |>
+    data.validator::validate_if(
+      lte(
+        wind_spd_mean_mps,
+        wind_spd_max_mps,
+        na_pass = TRUE
+      ),
+      description = "`wind_spd_mean_mps` ≤ `wind_spd_max_mps`"
+    ) |>
+    data.validator::validate_if(
+      btwn(
+        relative_humidity_mean,
+        relative_humidity_min,
+        relative_humidity_max,
+        na_pass = TRUE
+      ),
+      description = "`relative_humidity_*` (min ≤ mean ≤ max)"
+    ) |>
+    data.validator::validate_if(
+      btwn(
+        temp_soil_50cm_meanC,
+        temp_soil_50cm_minC,
+        temp_soil_50cm_maxC,
+        na_pass = TRUE
+      ),
+      description = "`temp_soil_50cm_*` (min ≤ mean ≤ max)"
+    ) |>
+    data.validator::validate_if(
+      btwn(
+        temp_soil_10cm_meanC,
+        temp_soil_10cm_minC,
+        temp_soil_10cm_maxC,
+        na_pass = TRUE
+      ),
+      description = "`temp_soil_10cm_*` (min ≤ mean ≤ max)"
+    ) |>
+    data.validator::add_results(report)
+  report
+  
+}
---
+++ app-bslib/R/format_report_gt.R
@@ -0,0 +1,108 @@
+library(gt)
+#' Convert `data.validator` report into custom `gt` table
+#'
+#' @param report a `data.validator` report object
+#' @param data original data frame used to generate the report
+#'
+#' @return a `gt` table
+format_report_gt <- function(report, data) {
+  #TODO make these arguments so they can be adjusted in the dashboard maybe?
+  #TODO would it be possible to add a header to the table that displays these thresholds?
+  warn_at <- 1 #warn if ≥ 1 row fails a validation
+  error_at <- 0.05 #error if ≥ 5% of rows fail validation
+  
+  data.validator::get_results(report) |> 
+    #for some reason, report has duplicated rows. Seems like a bug in data.validator
+    slice_head(n = 1, by = assertion.id) |> 
+    mutate(n_failed = if_else(is.na(num.violations), 0, num.violations),
+           p_failed = n_failed / nrow(data)) |> 
+    mutate(mark = case_when(
+      p_failed >= error_at ~ "🛑",
+      n_failed >= warn_at ~ "⚠️️",
+      .default = "✅"
+    )) |> 
+    mutate(bad_rows = map(error_df, ~dl_btn(.x, data))) |> 
+    select(assertion.id, mark, validation = description, n_failed, p_failed, bad_rows) |> 
+    mutate(bad_rows = ifelse(
+      is.na(bad_rows), #if no bad rows
+      "&mdash;", #print an emdash
+      make_csv_button(assertion.id, bad_rows) #else make tibble into a download button
+    )) |> 
+    gt() |> 
+    cols_hide(assertion.id) |> 
+    cols_label(
+      validation = "Validation",
+      n_failed = "Failed Rows",
+      p_failed = "% Failed",
+      bad_rows = "Download",
+      mark = ""
+    ) |>
+    tab_style(
+      cell_fill(color = "#FFB800B5"),
+      locations = cells_body(rows = n_failed >= warn_at)
+    ) |> 
+    tab_style(
+      style = list(
+        cell_fill(color = "#FF1212B2"),
+        cell_text(color = "white")
+      ),
+      locations = cells_body(rows = p_failed >= error_at)
+    ) |> 
+    cols_align(
+      align = "center",
+      columns = c(mark, bad_rows)
+    ) |> 
+    fmt_percent(p_failed) |> 
+    fmt_markdown(columns = c(validation, bad_rows))
+}
+
+#' helper function to turn the error_df column of a data.validator report into a
+#' list column of tibbles. The `index` column contains the row numbers of the
+#' original data that did not pass validations.
+dl_btn <- function(error_df, data) {
+  if(length(error_df$index) > 0) {
+    data |> 
+      dplyr::slice(error_df$index)
+  } else {
+    NA
+  }
+}
+
+
+#helper to reformat list column of tibbles to be html for a button that downloads the tibble as a .csv
+#adapted from pointblank package: https://github.com/rich-iannone/pointblank/blob/645698bc3ac1080f97ee14081d6cfe61a2c1d70f/R/get_agent_report.R#L1365
+
+make_csv_button <- function(assertion.id, bad_df) {
+  
+  map2_chr(assertion.id, bad_df, \(assertion.id, bad_df) {
+    
+    temp_file <- tempfile(pattern = paste0("csv_file_", assertion.id), fileext = ".csv")
+    utils::write.csv(bad_df, file = temp_file, row.names = FALSE)
+    on.exit(unlink(temp_file))
+    file_encoded <- base64enc::base64encode(temp_file)
+    output_file_name <- paste0(assertion.id, ".csv")
+    
+    as.character(
+      htmltools::a(
+        href = paste0("data:text/csv;base64,", file_encoded),
+        download = output_file_name,
+        htmltools::tags$button(
+          # `aria-label` = title_text, #tooltip?? not sure what this is
+          `data-balloon-pos` = "left",
+          style = htmltools::css(
+            `background-color` = "#67C2DC",
+            color = "#FFFFFF",
+            border = "none",
+            padding = "5px",
+            `font-weight` = "bold",
+            cursor = "pointer",
+            `border-radius` = "4px"
+          ),
+          "CSV"
+        )
+      )
+    )
+    
+  })
+}
+
